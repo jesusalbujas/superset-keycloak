1f0de6ad6a573dc83863b4f881eecb3e859b3ad9
jesusalbujas <jesusramirez35000@gmail.com>
2025-02-10 13:49:42 -0400

    Upgrade superset config

--- superset_config.py
+++ superset_config.py
@@ -1,24 +1,26 @@
 import os
+import logging
 from dotenv import load_dotenv
-from flask_appbuilder.security.manager import AUTH_OAUTH
+from flask_appbuilder.security.manager import AUTH_OID
 
-# Cargar variables desde .env
+# Configurar logging para depuración
+logging.basicConfig(level=logging.DEBUG)
+
 load_dotenv()
 
 SECRET_KEY = os.getenv('SUPERSET_SECRET_KEY', 'default-secret-key')
 
 # Habilitar OAuth
-AUTH_TYPE = AUTH_OAUTH
+AUTH_TYPE = AUTH_OID
 LOGOUT_REDIRECT_URL = f"{os.getenv('KEYCLOAK_SERVER')}/realms/{os.getenv('KEYCLOAK_REALM')}/protocol/openid-connect/logout"
 AUTH_USER_REGISTRATION = True
-AUTH_USER_REGISTRATION_ROLE = 'Gamma'
+AUTH_ROLES_SYNC_AT_LOGIN = True
 
-# Configuración de Keycloak
 OAUTH_PROVIDERS = [
     {
         'name': 'keycloak',
         'icon': 'fa-key',
-        'token_key': 'access_token',  # Keycloak usa 'access_token'
+        'token_key': 'access_token',
         'remote_app': {
             'client_id': os.getenv('KEYCLOAK_CLIENT_ID'),
             'client_secret': os.getenv('KEYCLOAK_CLIENT_SECRET'),
@@ -26,7 +28,13 @@ OAUTH_PROVIDERS = [
                 'scope': 'openid profile email',
             },
             'server_metadata_url': f"{os.getenv('KEYCLOAK_SERVER')}/realms/{os.getenv('KEYCLOAK_REALM')}/.well-known/openid-configuration",
-            'api_base_url': f"{os.getenv('KEYCLOAK_SERVER')}/realms/{os.getenv('KEYCLOAK_REALM')}/protocol/",
         },
     }
 ]
+
+# Asignación de roles de Keycloak a Superset
+AUTH_ROLES_MAPPING = {
+    "Superset_Admin": ["Admin"],
+    "Superset_Alpha": ["Alpha"],
+    "Superset_Gamma": ["Gamma"]
+}
